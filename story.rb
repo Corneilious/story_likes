5ca77bfd7bf9b5b502608a28e6a17814b2e66a1d
Neiland Fitzgerald <neilandfitzgerald@Neilands-MacBook-Air.local>
2016-03-27 12:01:35 -0400

    I added what the title and rating.

--- story.rb
+++ story.rb
@@ -1,3 +1,18 @@
+def title_and_rating (title,rating)
+puts ("The title of the film is #{title} and the rating is #{rating}")
+end
+
+puts ("What is the title?")
+user_title = gets.strip
+
+puts ("What is your rating of the movie out of ten?")
+user_rating = gets.strip
+
+
+title_and_rating(user_title, user_rating)
+
+
+
 require 'pry'
 
 #write a method that accepts two parameters: title and rating
@@ -11,5 +26,3 @@ require 'pry'
 #show the user a message asking for their rating of the movie out of ten
 
 # store the input in a variable
-
-display_story(user_title, user_rating)
